017e4e004d3b393c5eb1db0811ec9fc0eeea31e7
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-06-14 16:40:41 -0600

    Refactor: Clean up spcaing and indentation on Player class in player.js

--- src/player.js
+++ src/player.js
@@ -5,7 +5,6 @@ class Player {
     this.wins = 0;
     this.playerPiece = null;
   }
-
   identify(){
     if(this.name === "Your Computer"){
       computerName.innerText = this.name;
@@ -21,42 +20,40 @@ class Player {
         userAvatar.innerText = this.token;
     }
   }
-
   selectPiece(piece){
     this.playerPiece = piece;
   }
-
   takeTurn(event){
     if (this.name === "Your Computer"){
       var computerSelection = Math.floor(Math.random() * (2 - 0 + 1) + 0);
       var piece = newGame.classicGamePieces[computerSelection];
-        if (piece === 'rock') {
-          show(selectRightRock);
-        } else if (piece === 'paper') {
-          show(selectRightPaper);
-        } else if (piece === 'scissors') {
-          show(selectRightScissors);
-        }
-        newGame.rightPlayer.selectPiece(piece);
-      } else {
-        var piece = event.target.classList;
-        if (piece.contains('rock-selection')) {
-          show(selectLeftRock)
-          newGame.leftPlayer.selectPiece('rock');
-        } else if (piece.contains('paper-selection')) {
-          show(selectLeftPaper)
-          newGame.leftPlayer.selectPiece('paper');
-        } else if (piece.contains('scissors-selection')) {
-          show(selectLeftScissors)
-          newGame.leftPlayer.selectPiece('scissors');
-        }
+      if (piece === 'rock') {
+        show(selectRightRock);
+      } else if (piece === 'paper') {
+        show(selectRightPaper);
+      } else if (piece === 'scissors') {
+        show(selectRightScissors);
       }
+      newGame.rightPlayer.selectPiece(piece);
+    } else {
+      var piece = event.target.classList;
+      if (piece.contains('rock-selection')) {
+        show(selectLeftRock)
+        newGame.leftPlayer.selectPiece('rock');
+      } else if (piece.contains('paper-selection')) {
+        show(selectLeftPaper)
+        newGame.leftPlayer.selectPiece('paper');
+      } else if (piece.contains('scissors-selection')) {
+        show(selectLeftScissors)
+        newGame.leftPlayer.selectPiece('scissors');
+      }
+    }
   }
   takeTurnDifficultGame(event){
     if (this.name === "Your Computer"){
       var computerSelection = Math.floor(Math.random() * (4 - 0 + 1) + 0);
       var piece = newGame.difficultGamePieces[computerSelection];
-        if (piece === 'metal') {
+      if (piece === 'metal') {
           show(selectRightMetal);
         } else if (piece === 'water') {
           show(selectRightWater);
@@ -73,23 +70,19 @@ class Player {
         if (piece.contains('l-metal')) {
           show(selectLeftMetal)
           newGame.leftPlayer.selectPiece('metal');
-        } else if (piece.contains('l-water')) {
-          show(selectLeftWater)
-          newGame.leftPlayer.selectPiece('water');
-        } else if (piece.contains('l-wood')) {
-          show(selectLeftWood)
-          newGame.leftPlayer.selectPiece('wood');
-        } else if (piece.contains('l-fire')) {
-          show(selectLeftFire)
-          newGame.leftPlayer.selectPiece('fire');
-        }else if (piece.contains('l-earth')) {
-          show(selectLeftEarth)
-          newGame.leftPlayer.selectPiece('earth');
+          } else if (piece.contains('l-water')) {
+            show(selectLeftWater)
+            newGame.leftPlayer.selectPiece('water');
+          } else if (piece.contains('l-wood')) {
+            show(selectLeftWood)
+            newGame.leftPlayer.selectPiece('wood');
+          } else if (piece.contains('l-fire')) {
+            show(selectLeftFire)
+            newGame.leftPlayer.selectPiece('fire');
+          } else if (piece.contains('l-earth')) {
+            show(selectLeftEarth)
+            newGame.leftPlayer.selectPiece('earth');
         }
-      }
+    }
   }
-
-
-
-
 }
